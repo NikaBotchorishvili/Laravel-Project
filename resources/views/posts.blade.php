2b17ef752ad31b9d512b21e4f48e218567abf534
NikaBotchorishvili <nikabotchorishvili@gmail.com>
2022-04-04 23:14:23 +0400

    layouts v1

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,13 +1,6 @@
-<html>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/app.css">
-    <title>Blog</title>
-</head>
-<body>
+@extends("layout")
+
+@section("content")
      @if(!empty($posts))
         @foreach($posts as $post)
             <article>
@@ -19,5 +12,4 @@
             </article>
         @endforeach
     @endif
-</body>
-</html>
+@endsection
